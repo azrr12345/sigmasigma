5b53db785889d5c5c5174f26731efb0d223b829a
azrr12345 <alwi3023d@gmail.com>
2025-04-18 18:17:21 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let nama = 'azril' // tipe staring
+let usia = 16 //tipe integer number
+let tinggiBadan = 166.6 // tipe double float 
+let beratBadan = 60
+
+let tabungan = 50000
+let uangJajan = 30000
+const kebutuhan = 40000
+const uangAkhir = tabungan + uangJajan - kebutuhan
+
+
+// alert    (`nama saya ${nama} usia saya itu ${usia} tinggi badan saya adalah ${tinggiBadan}cm berat badan saya adalah ${beratBadan} saya adalah seorang pelajar`)
+alert   (`saya membuat web ini secara otodidak`)
+// alert   (`sekolah asal saya adalah mtsn 1 paser saya sekarang duduk di kelas 9`)
+alert    (`thxx dah buka`)
+alert   (`yg baca kek taii`)
